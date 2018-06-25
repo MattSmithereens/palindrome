312339462a0977fe6fce0240e9dcf0b8760d4181
Matt Smith and Thad Donaghue <matt@>
2018-06-25 15:29:05 -0700

    palindrome sniffer

---
+++ palindronium/ClassalC.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace palindronium
+{
+    public class ClassalC
+
+    {
+        private string inputtedText = "";
+        private char[] brokenWordArray;
+
+        public static void Main()
+        {
+            while (true)
+            {
+                ClassalC charClass = new ClassalC();
+                charClass.GetInputtedText();
+                charClass.PushInputToArray();
+                Console.WriteLine(charClass.IsPalindrome());
+            }
+
+        }
+
+        public void GetInputtedText()
+
+        {
+            Console.WriteLine("Enter text to test for palindrome-ness!");
+            inputtedText = Console.ReadLine();
+
+        }
+
+        public void PushInputToArray()
+        {
+            brokenWordArray = inputtedText.ToCharArray();
+
+        }
+
+        public string IsPalindrome()
+        {
+            char[] reversedArray = inputtedText.ToCharArray();
+
+            Array.Reverse(reversedArray);
+
+            string response = "This is a palindrome";
+
+            for (int i = 0; i < brokenWordArray.GetLength(0); i++) {
+                if(brokenWordArray[i] != reversedArray[i]) {
+                    response = "This is not a palindrome!";
+                    break;
+                } 
+            }
+
+            return response;
+
+        }
+
+    }
+
+}
